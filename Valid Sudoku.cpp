73ffe6c967ea941cf68a99c5a464267786227da8
Parth02-07 <202401215@daiict.ac.in>
2025-04-03 14:00:36 +0530

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
          vector<set<char>> rows(9), cols(9), boxes(9);
-        
         for (int i=0;i<9;i++) {
             for (int j=0;j<9;j++){
                 char num=board[i][j];
-                if (num=='.')continue;
-                
+                if (num=='.')
+                    continue;
                 int boxIndex=(i/3)*3+(j/3);
-                
                 if (rows[i].count(num)||cols[j].count(num)|| boxes[boxIndex].count(num)) {
                     return false;
                 }
@@ -20,5 +18,4 @@ public:
         }
         return true;
     }
-    
 };
